d2340264bf138e7a258d52f455a771b52672fc03
github username <github email address>
2019-01-09 18:56:18 -0500

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+require 'pry'
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |k, v|
+      v.sort!
+    end
+  end
+
+
+end
+# bayside = School.new("bayside")
+# binding.pry
+# 0
